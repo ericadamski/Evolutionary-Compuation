26304aa632402e83f0d43a69c2662924c710f2ef
Eric Adamski <er.adamski@gmail.com>
2014-12-02 22:21:56 -0500

    see other commit msg still need to add simplificatin of formulas

--- Genetic-Algorithms-Project/src/Formula.java
+++ Genetic-Algorithms-Project/src/Formula.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class Formula
 {
@@ -14,15 +15,33 @@ public class Formula
     {
       if (term.equals(""))
       {
-        complete = false;
-        break;
+        return false;
       }
-      if (term.split("=").length == 2)
+
+      String equals[] = term.split("=");
+
+      if (equals.length == 2)
+      {
         complete = true;
+        try
+        {
+          m_answer = Integer.parseInt(equals[1]);
+          terms[Arrays.asList(terms).indexOf(term)] = equals[0];
+        }
+        catch (Exception e)
+        {
+          return false;
+        }
+      }
     }
+
+    if (!complete)
+      return complete;
+
     m_formula = formula;
     m_terminals = terms;
-    return complete;
+
+    return splitTerminals(terms);
   }
 
   public static void improperFormulaErrorMsg(String error)
@@ -37,8 +56,80 @@ public class Formula
   public String[] getTerminals()
   { return m_terminals; }
 
+  public int getAnswer()
+  { return m_answer; }
+
+  public int[] getOrderedCoefficents()
+  { return m_coefficents; }
+
+  public char[] getOrderedVariables()
+  { return m_variables; }
+
+  public int getNoncoefficents()
+  { return m_noncoefficents; }
+
+  private boolean splitTerminals(String terminals[])
+  {
+    // pairs of the form NUMBERletter and NUMBER and ?=?
+    int terminalLength = 0;
+    for (String term : terminals)
+    {
+      String splitTerm[] = term.split("(?<=(\\D+))|(?=(\\D+))");
+      if ( splitTerm.length > 1 )
+        ++terminalLength;
+    }
+    int terminalCount = 0;
+    m_variables = new char[terminalLength];
+    m_coefficents = new int[terminalLength];
+    for (String term : terminals)
+    {
+      int num;
+      String var = "";
+
+      String splitTerm[] = term.split("(?<=(\\D+))|(?=(\\D+))");
+
+      switch (splitTerm.length)
+      {
+        case 1:
+          //NUMBER
+          try
+          {
+            m_noncoefficents += Integer.parseInt(splitTerm[0]);
+          }
+          catch (Exception e)
+          {
+            return false;
+          }
+          break;
+
+        case 2:
+          //NUMBER<VAR>
+          try
+          {
+            m_coefficents[terminalCount] = Integer.parseInt(splitTerm[0]);
+            m_variables[terminalCount] = splitTerm[1].charAt(0);
+          }
+          catch (Exception e)
+          {
+            return false;
+          }
+          break;
+
+        default:
+          return false;
+      }
+      ++terminalCount;
+    }
+    return true;
+  }
+
   private String m_formula;
-  private String m_terminals[];
+  private String m_terminals[]; // numberletter pairs
+
+  private int m_coefficents[]; // number
+  private char m_variables[]; // letter
+  private int m_noncoefficents;
+  private int m_answer;
 
   public static void main(String[] args)
   {
@@ -50,6 +141,12 @@ public class Formula
       {
         if ( !parser.parse(formula) )
           improperFormulaErrorMsg(formula);
+
+        for ( int i : parser.getOrderedCoefficents() )
+          System.out.println(i);
+        for ( char c : parser.getOrderedVariables() )
+          System.out.println(c);
+        System.out.println(parser.getNoncoefficents());
       }
       else
         improperFormulaErrorMsg(formula);
